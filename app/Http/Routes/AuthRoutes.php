3c12f5fe2528f83069491a32cd4adfd156d36998
张龙 <zhanglong@hoge.cn>
2017-05-10 11:28:13 +0800

    cehsi

--- app/Http/Routes/AuthRoutes.php
+++ app/Http/Routes/AuthRoutes.php
@@ -14,9 +14,9 @@ class AuthRoutes{
         $api->version(env('API_VERSION'), function ($api) {
             $api->group(['namespace' => 'App\Http\Controllers\Api\Internal'], function ($api) {
                 $api->group(['namespace' => 'Auth'], function ($api) {
-
+                    $api->group(['prefix' => 'adv'],function($api){
                         $api->get('adv', 'AuthController@index');                        // 获取广告列表
-
+                });
 
                 });
             });
